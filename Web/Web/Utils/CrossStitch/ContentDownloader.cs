f4ae6bdf690f7437838f8e0e257bc290c92cafff
Olga Shilo <olga.shilo@hiqo-solutions.com>
2018-11-01 15:03:23 +0300

    Pattern updates page: refactor downloading

---
+++ Web/Web/Utils/CrossStitch/ContentDownloader.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Web.Utils.CrossStitch
+{
+    public abstract class ContentDownloader
+    {
+        public async Task<string> Download(Uri uri)
+        {
+            using (var client = new HttpClient())
+            {
+                var response = await client.GetAsync(uri.AbsoluteUri);
+                if (response != null && response.StatusCode == HttpStatusCode.OK)
+                {
+                    var content = await response.Content.ReadAsStringAsync();
+                    return content;
+                }
+            }
+            return string.Empty;
+        }
+    }
+}
---
+++ Web/Web/Utils/CrossStitch/UpdatesDownloader.cs
@@ -0,0 +1,26 @@
+﻿using AngleSharp.Parser.Html;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Web.Models.CrossStitch;
+
+namespace Web.Utils.CrossStitch
+{
+    public class UpdatesDownloader : ContentDownloader
+    {
+        public async Task Parse()
+        {
+            var uri = new Uri("https://www.stitch.su/patterns?favch=4&page=3&lim=50");
+            var models = new List<StitchSuPatternModel>();
+            var content = await Download(uri);
+            var domParser = new HtmlParser();
+            using (var document = await domParser.ParseAsync(content))
+            {
+
+                var divNodes = document.All.Where(item => item.LocalName == "div").Where(item => item.ClassName != null && item.ClassName == "set");
+                models.AddRange(divNodes.Select(item => StitchSuPatternModel.Parse(item, uri)));
+            }
+        }
+    }
+}
