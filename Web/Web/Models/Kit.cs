fb0e35e96943d81c39b8ab9c7a0e066bce0a7573
Olga Shilo <olga.shilo@hiqo-solutions.com>
2018-12-17 11:29:47 +0300

    Add three-column layout for owned kits

---
+++ Web/Web/Models/Kit.cs
@@ -0,0 +1,24 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace Web.Models
+{
+    public enum KitType
+    {
+        ManufacturerKit,
+        DesignerPattern
+    }
+
+    public class Kit
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+        public string Title { get; set; }
+        public string Manufacturer { get; set; }
+        public string Item { get; set; }
+        public string Size { get; set; }
+        public string ImageUrl { get; set; }
+        public KitType KitType { get; set; }
+    }
+}
